31a38bcc42ec738664f51474d275ab575a493487
arilobi <arinabulobi@gmail.com>
2024-11-04 11:30:27 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -15,9 +15,9 @@ const HogTile = ({ hog, onClick}) => (
 const HogDetails = ({ hog }) => (
   <div className="hog-details">
     <p><strong>Specialty:</strong>{hog.specialty}</p>
-    <p><strong>Weight</strong>{hog.weight}</p>
-    <p><strong>Greased</strong>{hog.greased ? 'Yes' : 'No'}</p>
-    <p><strong>Highest Medal Achieved</strong>{hog["highest medal achieved"]}</p>
+    <p><strong>Weight:</strong>{hog.weight}</p>
+    <p><strong>Greased:</strong>{hog.greased ? 'Yes' : 'No'}</p>
+    <p><strong>Highest Medal Achieved:</strong>{hog["highest medal achieved"]}</p>
   </div>
 );
 
